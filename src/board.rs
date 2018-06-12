c04a51342c5c93a9b4bbb8c53ba286d0cb47f9a8
Jonir Rings <peterpuyi@live.cn>
2018-06-13 02:05:05 +0800

    extract neighbor calculation

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,5 @@
 use std::fmt::{self, Display, Formatter};
 
-use ndarray::Array;
 use ndarray::Array2;
 
 type Grid = Array2<u8>;
@@ -10,55 +9,55 @@ pub struct Board {
     rows: usize,
     cols: usize,
     grid: Grid,
-    scratch: Grid,
 }
 
 impl Board {
     pub fn new(rows: usize, cols: usize, cells: Vec<u8>, generation: usize) -> Board {
-        // make a border of dead cells, 0 as dead, 1 as alive
-        let mut grid = Grid::from_elem(((rows + 2), (cols + 2)), 0);
-        let scratch = Grid::zeros((rows, cols));
-        let a = Array::from_vec(cells).into_shape((rows, cols)).unwrap();
-        grid.slice_mut(s![1..-1, 1..-1]).assign(&a);
+        let grid = Grid::from_shape_vec((rows, cols), cells).unwrap();
         Board {
             generation,
             rows,
             cols,
             grid,
-            scratch,
         }
     }
+    fn calc_neighbors(&self) -> Grid {
+        let ref grid = self.grid;
+        // make a border of dead cells, 0 as dead, 1 as alive
+        let mut expanded_grid = Grid::from_elem(((self.rows + 2), (self.cols + 2)), 0);
+        expanded_grid.slice_mut(s![1..-1,1..-1]).assign(&grid);
+
+        let mut scratch = Grid::zeros((self.rows, self.cols));
+        {
+            // take eight directions as views,
+            // add up all views into the scratch board,
+            // then the value in every scratch board slot
+            // is the number of the slot's alive neighbors
+            let mut neighbor_view = scratch.view_mut();
+            neighbor_view += &expanded_grid.slice(s![0..-2, 0..-2]); // north west
+            neighbor_view += &expanded_grid.slice(s![0..-2, 1..-1]); // west
+            neighbor_view += &expanded_grid.slice(s![0..-2, 2..  ]); // south west
+
+            neighbor_view += &expanded_grid.slice(s![1..-1, 0..-2]); // north
+            neighbor_view += &expanded_grid.slice(s![1..-1, 2..  ]); // south
+
+            neighbor_view += &expanded_grid.slice(s![2.., 0..-2]); // north east
+            neighbor_view += &expanded_grid.slice(s![2.., 1..-1]); // east
+            neighbor_view += &expanded_grid.slice(s![2.., 2..  ]); // south east
+        }
+        scratch
+    }
     pub fn iterate(&mut self) {
-        let ref mut grid = self.grid;
-        let ref mut scratch = self.scratch;
-        let mut neigh = scratch.view_mut();
-
-        neigh.fill(0);
-        // take eight directions as views,
-        // add up all views in a scratch board,
-        // then the value in every scratch board slot is the number of its alive neighbors
-        neigh += &grid.slice(s![0..-2, 0..-2]); // north west
-        neigh += &grid.slice(s![0..-2, 1..-1]); // west
-        neigh += &grid.slice(s![0..-2, 2..]); // south west
-
-        neigh += &grid.slice(s![1..-1, 0..-2]); // north
-        neigh += &grid.slice(s![1..-1, 2..]); // south
-
-        neigh += &grid.slice(s![2.., 0..-2]); // north east
-        neigh += &grid.slice(s![2.., 1..-1]); // east
-        neigh += &grid.slice(s![2.., 2..]); // south east
-
-        let mut grid_view = grid.slice_mut(s![1..-1, 1..-1]); // central grid view
-
+        let neighbor_grid = self.calc_neighbors();
+        self.grid.zip_mut_with(&neighbor_grid, |y: &mut u8, &n: &u8| {
+            *y = match (*y, n) {
+                (_, 3) | (1, 2) => 1,
+                _ => 0,
+            }
+        });
         // 2 or 3 neighbors: stay alive
         // 3 neighbors: birth
         // otherwise: death
-        grid_view.zip_mut_with(&neigh, |y: &mut u8, &n: &u8| {
-            *y = match (*y, n) {
-                (_, 3) | (1, 2) => 1,
-                (_, _) => 0,
-            }
-        });
         self.generation += 1;
     }
 }
